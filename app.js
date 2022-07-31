f5f10afdf877b923062fd4724c3ae6526d8d769d
Glitch (hello-express) <none>
2022-07-31 18:15:06 +0000

    ☃️🎄 Checkpoint ./app.js:44950819/2275

--- app.js
+++ app.js
@@ -2,9 +2,64 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.SLACK_STATE_SECRET,
+  scopes: ["chat:write", "users:read"],
+  installationStore: {
+    storeInstallation: async (installation) => {
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        // handle storing org-wide app installation
+        return await saveUserOrgInstall(installation);
+      }
+      if (installation.team !== undefined) {
+        // single team app installation
+        return await saveUserWorkspaceInstall(installation);
+      }
+      throw new Error("Failed saving installation data to installationStore");
+    },
+    fetchInstallation: async (installQuery) => {
+      // Bolt will pass your handler an installQuery object
+      // Change the lines below so they fetch from your database
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        // handle org wide app installation lookup
+        return await getEnterpriseInstallation(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation lookup
+        return await getWorkspaceInstallation(installQuery.teamId);
+      }
+      throw new Error("Failed fetching installation");
+    },
+    deleteInstallation: async (installQuery) => {
+      // Bolt will pass your handler  an installQuery object
+      // Change the lines below so they delete from your database
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        // org wide app installation deletion
+        return await deleteEnterpriseInstallation(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation deletion
+        return await deleteWorkspaceInstallation(installQuery.teamId);
+      }
+      throw new Error("Failed to delete installation");
+    },
+  },
+  installerOptions: {
+    directInstall: true,
+  },
 });
+
 
 
 
