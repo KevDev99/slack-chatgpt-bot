b7a012d01d1f073664e252677c26829f36c1ed31
Glitch (hello-express) <none>
2022-07-31 19:02:58 +0000

    🏮🐸 Checkpoint ./listeners/events/app_home_opened.js:44950819/2 ./database/models/userModel.js:44950819/664

---
+++ database/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    _id: String,
+    user_tz: String,
+    user_tz_hours: Number,
+    start_time: Number,
+    end_time: Number,
+    interval: Number,
+    team: { id: String, name: String },
+    enterprise: { id: String, name: String },
+    user: { token: String, scopes: [String], id: String },
+    tokenType: String,
+    isEnterpriseInstall: Boolean,
+    appId: String,
+    authVersion: String,
+    countryCode: String,
+    bot: {
+      scopes: [String],
+      token: String,
+      userId: String,
+      id: String,
+    },
+  },
+  { _id: false }
+);
+
+module.exports = mongoose.model("User", userSchema);
