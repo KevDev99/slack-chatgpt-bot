d76116435549eb3e947201eb83f92c3835d820bb
Glitch (maroon-maize-fedora) <none>
2022-08-02 14:12:56 +0000

    🎁🚓 Checkpoint ./listeners/events/delete_todo.js:44950819/11744 ./package.json:44950819/974

--- listeners/events/delete_todo.js
+++ listeners/events/delete_todo.js
@@ -1,60 +1,62 @@
 const { deleteTask, getTask } = require("../../database/db.js");
 const { getAppHome } = require("./app_home_opened.js");
 
+const moment = require("moment");
+
 const deleteTodo = async ({ ack, body, client }) => {
   try {
     // send "clear" signal as response action to close the modal in slack
     await ack();
-
-
 
     // check if there is an external_id provided -> submit is an edit form
     const task_id = body.view.private_metadata;
 
     // get user id from body
     const userId = body["user"]["id"];
-    
+
     const task = await getTask(task_id);
 
     if (task_id) {
       await deleteTask(task_id);
     }
-    
-    // inform user about deletion
-    client.chat.postMessage({
-          channel: task.assigned_user,
-          blocks: [
-            {
-              type: "section",
-              text: {
-                type: "plain_text",
-                text: "❌ Following task has been deleted:",
-                emoji: true,
+
+    // inform assigned user about deletion
+    if (task.assigned_user) {
+      client.chat.postMessage({
+        channel: task.assigned_user,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: " Following task has been deleted:",
+              emoji: true,
+            },
+          },
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: task.summary,
+              emoji: true,
+            },
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: "By: " + `<@${userId}>`,
               },
-            },
-            {
-              type: "header",
-              text: {
-                type: "plain_text",
-                text: task.summary,
-                emoji: true,
+              {
+                type: "mrkdwn",
+                text: "at: " + moment().format("YYYY-MM-DD, h:mm:ss a"),
               },
-            },
-            {
-              type: "context",
-              elements: [
-                {
-                  type: "mrkdwn",
-                  text: "From: " + `<@${userId}>`,
-                },
-                {
-                  type: "mrkdwn",
-                  text: "From: " + `<@${userId}>`,
-                },
-              ],
-            },
-          ],
-        });
+            ],
+          },
+        ],
+      });
+    }
 
     // update view
     client.views.publish({
