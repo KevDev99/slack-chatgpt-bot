af56e725bc3feb581273f04486c24249926a7bc5
Glitch (maroon-maize-fedora) <none>
2022-09-06 18:34:11 +0000

    🚿🚌 Checkpoint ./listeners/actions/export_tasks.js:44950819/2484

--- listeners/actions/export_tasks.js
+++ listeners/actions/export_tasks.js
@@ -1,9 +1,27 @@
+const { fetchTasks } = require("../../database/db.js");
 
 const exportTasks = async ({ ack, say, body, client }) => {
   await ack();
 
   try {
+    let outPutText = "";
+    // fetch all tasks
+    const tasks = await fetchTasks("done", body.user.id);
     
+    tasks.map(async (task) => {
+      let assigned_user;
+
+      if(task.assigned_user) {
+        assigned_user = await client.users.profile.get({user: task.assigned_user})
+      }
+      
+      outPutText += `${task.summary} ${assigned_user? `- ${assigned_user}` : ""}`;
+      outPutText += "\n--------------------------------------------------------------------------\n"
+    })
+    
+    console.log(outPutText);
+
+    // export to text file
   } catch (error) {
     console.error(error);
   }
