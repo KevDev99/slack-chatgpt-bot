bb5c077f8b952b9977ae585c74205ff0d71d5f87
Glitch (maroon-maize-fedora) <none>
2022-10-04 15:07:16 +0000

    💁🚐 Checkpoint ./listeners/events/index.js:44950819/858

--- listeners/events/index.js
+++ listeners/events/index.js
@@ -1,8 +1,9 @@
 const { appHomeOpened } = require("./app_home_opened.js");
 const { setHabitTarget } = require("./set_habit_target.js");
+const { submitHabit } = require("./submit_habit.js");
 
 module.exports.register = (app) => {
   app.event("app_home_opened", appHomeOpened);
   app.view("set_habit_target", setHabitTarget);
+  app.view("submit_habit", submitHabit);
 };
-
