e3b297b10fc2b8931643bd2cc4b5bb5e5553c3f4
Glitch (bodzii-slack-app) <none>
2022-12-18 11:50:38 +0000

    😂❄️ Checkpoint ./listeners/events/set_dial_pad_status.js:44950819/7006

--- listeners/events/set_dial_pad_status.js
+++ listeners/events/set_dial_pad_status.js
@@ -2,6 +2,8 @@ const setDialPadStatus = async ({ message, client, say }) => {
   const { text } = message;
   const textParts = text.split("\n");
   const { members } = await client.users.list();
+  
+  console.log(message);
 
   textParts.map((textPart) => {
     if (!textPart.includes("Handled by")) {
@@ -11,25 +13,32 @@ const setDialPadStatus = async ({ message, client, say }) => {
     const handledByFields = textPart.split(" ");
 
     const validFilterField = [];
-    let user;
 
     handledByFields.map((field) => {
       const filteredMembers = members.filter((member) =>
         member.profile.real_name.includes(field)
       );
-      
-      if (filteredMembers.length > 1) {
+
+      if (filteredMembers.length >= 1) {
         return validFilterField.push(field);
       }
+    });
 
-      if (filteredMembers.length == 1) {
-        user = field;
+    const foundMembers = [];
+
+    members.map((member) => {
+      let matchYN = new RegExp(validFilterField.join("|")).test(
+        member.profile.real_name
+      );
+
+      if (matchYN) {
+        foundMembers.push(member);
       }
     });
+
+    let user = foundMembers[0];
     
-    console.log(validFilterField);
-    
-    console.log(user);
+
   });
 };
 
