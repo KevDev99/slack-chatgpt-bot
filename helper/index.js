efd0a12a88c47e663f6e983e646bb7488b05a0ee
Glitch (bodzii-slack-app) <none>
2023-01-06 10:03:58 +0000

    🌷🎩 Checkpoint ./helper/index.js:44950819/7624 ./listeners/events/set_dial_pad_status.js:44950819/1616

--- helper/index.js
+++ helper/index.js
@@ -59,45 +59,53 @@ const insertAt = (array, index, ...elementsArray) => {
 };
 
 async function getUserFromTextBody(textParts, client) {
-  let user;
+  try {
+    let user;
 
-  const members = await getUsers();
-  
-  textParts.map((textPart) => {
+    const members = await getUsers();
 
-    const handledByFields = textPart.split(" ");
+    textParts.map((textPart) => {
+      const handledByFields = textPart.split(" ");
 
-    const validFilterField = [];
-  
+      const validFilterField = [];
 
-    handledByFields.map((field) => {
-      const filteredMembers = members.filter((member) =>
-        member.user.name.includes(field)
-      );
+      handledByFields.map((field) => {
+        const filteredMembers = members.filter((member) =>
+          member.user.name.includes(field)
+        );
 
-      if (filteredMembers.length >= 1) {
-        return validFilterField.push(field);
-      }
+        if (filteredMembers.length >= 1) {
+          return validFilterField.push(field);
+        }
+      });
+
+      const foundMembers = [];
+
+      members.map((member) => {
+        let matchYN = new RegExp(validFilterField.join("|")).test(
+          member.user.name
+        );
+
+        if (matchYN) {
+          foundMembers.push(member);
+        }
+      });
+      user = foundMembers[0];
     });
 
-    const foundMembers = [];
-
-    members.map((member) => {
-      let matchYN = new RegExp(validFilterField.join("|")).test(
-        member.user.name
-      );
-
-      if (matchYN) {
-        foundMembers.push(member);
-      }
-    });
-    user = foundMembers[0];
-  });
-
-  return user;
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function setUserStatus(client, user, statusText, statusEmoji) {
+async function setUserStatus(
+  client,
+  user,
+  statusText,
+  statusEmoji,
+  statusExpirationUnix
+) {
   try {
     // check if user exists in db
     const dbUser = await getUser(user.id);
@@ -107,15 +115,25 @@ async function setUserStatus(client, user, statusText, statusEmoji) {
       return console.log("User not in db", user);
       // TODO: send notification to authenticate the app
     }
-    
+
+    const statusObj = {
+      status_text: statusText,
+      status_emoji: statusEmoji,
+    };
+
+    if (statusExpirationUnix) {
+      statusObj.status_expiration = statusExpirationUnix;
+    }
+
+    console.log(statusObj);
+
     // set status for user
     const { ok, error, profile } = await client.users.profile.set({
       token: dbUser.user.token,
-      profile: {
-        status_text: statusText,
-        status_emoji: statusEmoji,
-      },
+      profile: statusObj,
     });
+
+   
 
     if (!ok) throw error;
   } catch (err) {
@@ -123,9 +141,17 @@ async function setUserStatus(client, user, statusText, statusEmoji) {
   }
 }
 
+function getTimestampInSeconds(date, minutes = 0) {
+  // check if minutes need to be added
+  let timestampDate = new Date(date.getTime() + minutes * 60000);
+
+  return Math.floor(timestampDate / 1000);
+}
+
 module.exports = {
   formatState,
   formatMessageState,
   getUserFromTextBody,
   setUserStatus,
+  getTimestampInSeconds,
 };
--- listeners/events/set_dial_pad_status.js
+++ listeners/events/set_dial_pad_status.js
@@ -1,33 +1,34 @@
-const { getUserFromTextBody, setUserStatus } = require("../../helper");
-const {getUsers} = require('../../database/db.js')
+const { getUserFromTextBody, setUserStatus, getTimestampInSeconds } = require("../../helper");
 
 const setDialPadStatus = async ({ message, client, say }) => {
   try {
-    const { attachments } = message;
     
+    console.log(message);
+   //  const { attachments } = message;
 
-    if(!attachments) return;
-    
-    const text = attachments[2].text;
-    
-    console.log(text);
-    
-    
+    // if (!attachments) return;
+
+    const text = message.text //attachments[2].text;
+
     if (!text.includes("Handled by")) {
       return;
     }
-    
+
     const textParts = text.split("\n");
 
     const user = await getUserFromTextBody(textParts, client);
-    
-    console.log(user);
-    
-    if(!user) {
+
+    console.log("set status of ", user);
+
+    if (!user) {
       return;
     }
 
-    setUserStatus(client, user, "In a call", "☎️");
+    // set status in a call
+
+    // get unix timestamp for expiration
+    const unixTimestamp = getTimestampInSeconds(new Date(), 15);
+    setUserStatus(client, user, "In a call", "☎️", unixTimestamp);
   } catch (err) {
     console.error(err);
   }
