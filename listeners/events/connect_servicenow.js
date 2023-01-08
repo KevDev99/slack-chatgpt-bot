3dfcd0199bd271b8292133581e56f8b4bb14961c
Glitch (bodzii-slack-app) <none>
2023-01-08 16:58:22 +0000

    ✨👝 Checkpoint ./listeners/events/connect_servicenow.js:44950819/11163

--- listeners/events/connect_servicenow.js
+++ listeners/events/connect_servicenow.js
@@ -8,32 +8,44 @@ const connectServiceNow = async ({ body, client, logger, ack }) => {
 
     try {
       // check if instance url is reachable
+      // check if instance url + client id is correct
       const instanceRes = await axios(
-        `${state.instance_url}/oauth_auth.do?response_type=code&redirect_uri=https://slack-servicenow.glitch.me/snow_oauth_redirect&client_id=a60633d0d2986110e6aad8c0b956804e&state=xyzABC123`
+        `${state.instance_url}/oauth_auth.do?response_type=code&redirect_uri=${process.env.REDIRECT_URL}&client_id=${state.client_id}&state=xyzABC123`
       );
 
+      // acknowledge request
+      await ack();
+      
+      // update values in db
+      
       
     } catch (err) {
-      if (err.code && err.code === "ENOTFOUND") {
-        // return url is wrong
-        return ack({
-          response_action: "errors",
-          errors: {
-            instance_url: "The URL is not reachable. Please make sure it's valid.",
-          },
-        });
+      if (err.code) {
+        if (err.code === "ENOTFOUND") {
+          // return url is wrong
+          return ack({
+            response_action: "errors",
+            errors: {
+              instance_url:
+                "The URL is not reachable. Please make sure it's valid.",
+            },
+          });
+        }
+        if (err.code === "ERR_BAD_REQUEST") {
+          // return url is wrong
+          return ack({
+            response_action: "errors",
+            errors: {
+              client_id:
+                "The Client ID doesn't seem to be correct. Please check it again.",
+            },
+          });
+        }
       }
-      
+
       console.error(err);
     }
 
-    // check if instance url + client id is correct
-
-    // acknowledge request
-
-    // refresh home screen
-
-    //await ack();
   } catch (err) {
     console.error(err);
   }
