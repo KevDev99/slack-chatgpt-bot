73b1821bb64e4051419089841064efe5bcdd18a6
Glitch (bodzii-slack-app) <none>
2023-01-08 16:22:23 +0000

    🚜❤️ Checkpoint ./listeners/events/connect_servicenow.js:44950819/451

--- listeners/events/connect_servicenow.js
+++ listeners/events/connect_servicenow.js
@@ -1,8 +1,15 @@
 const connectServiceNow = async ({ body, client, logger, ack }) => {
   try {
-    // ...
-    console.log(body.view.state.values);
-
+    // format body state
+    
+    // check if instance url is reachable
+    
+    // check if instance url + client id is correct
+    
+    // acknowledge request
+    
+    // refresh home screen
+    
     //await ack();
   } catch (err) {
     console.error(err);
