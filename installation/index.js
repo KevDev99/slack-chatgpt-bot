e1ca0d8329d5cc43e5a55f68edbf0a0ca0c102b5
Glitch (bodzii-slack-app) <none>
2023-01-20 10:30:52 +0000

    ⏱💗 Checkpoint ./installation/index.js:44950819/9046 ./listeners/events/app_home_opened.js:44950819/1561

--- installation/index.js
+++ installation/index.js
@@ -87,6 +87,57 @@ const sendWelcomeMessage = async (installation) => {
             text: "This app enables you to easily forward messages, files, ... as mail.",
           },
         },
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "How To Send Mails:",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Click on the *'more actions' menu* on a message",
+          },
+        },
+        {
+          type: "image",
+          title: {
+            type: "plain_text",
+            text: " ",
+            emoji: true,
+          },
+          image_url:
+            "https://www.linkpicture.com/q/Screenshot-2023-01-20-at-11.08.14.png",
+          alt_text: "marg",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Click on the *Shortcut* 'Send as Mail'",
+          },
+        },
+        {
+          type: "image",
+          image_url:
+            "https://www.linkpicture.com/q/Screenshot-2023-01-20-at-11.13.27.png",
+          alt_text: "inspiration",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "All *documents, images, ... will be automatically attatched* to the mail. The message text is automatically added to the message input on the modal:",
+          },
+        },
+        {
+          type: "image",
+          image_url:
+            "https://www.linkpicture.com/q/Screenshot-2023-01-20-at-11.15.26.png",
+          alt_text: "inspiration",
+        },
       ],
       text: "Let’s get started!",
     },
