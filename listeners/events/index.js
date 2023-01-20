61cc09383b147c56ef7684b559a1998ea529a0f0
Glitch (bodzii-slack-app) <none>
2023-01-20 10:07:27 +0000

    🐪💽 Checkpoint ./listeners/events/index.js:44950819/632 ./listeners/events/app_home_opened.js:44950819/4817

--- listeners/events/index.js
+++ listeners/events/index.js
@@ -1,5 +1,7 @@
 const submitSendMail = require("./submit_sendmail.js");
+const appHomeOpened = require("./app_home_opened.js");
 
 module.exports.register = (app) => {
   app.view("submit_sendmail", submitSendMail);
+  app.event("app_home_opened", appHomeOpened);
 };
