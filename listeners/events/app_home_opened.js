2cffd2eaf86b845a5a01a9d2f0777fbc25daf48f
Glitch (bodzii-slack-app) <none>
2023-01-23 21:09:42 +0000

    🏐⛵️ Checkpoint ./listeners/events/app_home_opened.js:44950819/13218

--- listeners/events/app_home_opened.js
+++ listeners/events/app_home_opened.js
@@ -7,55 +7,68 @@ const appHomeOpened = async ({ event, client }) => {
       view: {
         // Home tabs must be enabled in your app configuration page under "App Home"
         type: "home",
-        blocks: [
-          {
-            type: "header",
-            text: {
-              type: "plain_text",
-              text: "Users",
-              emoji: true,
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "kevin.taufer@outlook.com",
-            },
-            accessory: {
-              type: "button",
-              text: {
-                type: "plain_text",
-                text: "Remove",
-                emoji: true,
-              },
-              value: "kevin.taufer@outlook.com",
-              action_id: "remove_user",
-            },
-          },
-          {
-            type: "divider",
-          },
-          {
-            dispatch_action: true,
-            block_id: "add_user",
-            type: "input",
-            element: {
-              type: "plain_text_input",
-              action_id: "add_user-action",
-            },
-            label: {
-              type: "plain_text",
-              text: "New user",
-              emoji: true,
-            },
-          },
-        ],
+        blocks: getAppHomeBlocks(),
       },
     });
   } catch (error) {
     console.error(error);
   }
+};
+
+const getAppHomeBlocks = () => {
+  const blocks = [
+    {
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: "Users",
+        emoji: true,
+      },
+    },
+  ];
+  
+  // load users
+  const users = 
+
+  blocks.push({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "kevin.taufer@outlook.com",
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        text: "Remove",
+        emoji: true,
+      },
+      value: "kevin.taufer@outlook.com",
+      action_id: "remove_user",
+    },
+  });
+
+  blocks.push(
+    {
+      type: "divider",
+    },
+    {
+      dispatch_action: true,
+      block_id: "add_user",
+      type: "input",
+      element: {
+        type: "plain_text_input",
+        action_id: "add_user-action",
+      },
+      label: {
+        type: "plain_text",
+        text: "New user",
+        emoji: true,
+      },
+    }
+  );
+
+  return blocks;
 };
 
 module.exports = appHomeOpened;
