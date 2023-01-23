3e5f4d943bc19ff747cf1f939c75655a242cfc28
Glitch (bodzii-slack-app) <none>
2023-01-23 19:59:14 +0000

    💑😹 Checkpoint ./listeners/actions/add_user.js:44950819/2775

--- listeners/actions/add_user.js
+++ listeners/actions/add_user.js
@@ -1,14 +1,26 @@
 const { formatState } = require("../../helper");
 
+const mail_regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
 const addUser = async ({ body, client, ack, shortcut, say }) => {
   try {
-    await ack();
-
     // format state
     const state = formatState(body.view.state.values);
-    
+
     // check if input is a mail
+    const { add_user } = state;
+    if (!mail_regex.test(add_user)) {
+      return  ack({
+        response_action: "errors",
+        errors: {
+          add_user: "Please enter a valid email.",
+        },
+      });
+    }
     
+    
+
+    await ack();
   } catch (err) {
     console.error(err);
   }
