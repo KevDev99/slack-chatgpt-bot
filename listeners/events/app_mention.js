f9b94f7bbc08c939bf866aa1af19215a47c60ca8
Glitch (bodzii-slack-app) <none>
2023-03-26 13:22:37 +0000

    🚥🚋 Checkpoint ./installation/index.js:44950819/4543 ./listeners/events/app_mention.js:44950819/418

--- listeners/events/app_mention.js
+++ listeners/events/app_mention.js
@@ -3,7 +3,18 @@ const { getUsers } = require("../../database/db.js");
 const appMention = async ({ event, client, body }) => {
 
   try {
-    console.log(event);
+    
+    if(!event || !event.text) {
+      console.error("event text not provided or empty")
+      return;
+    }
+    // get text
+    const text = event.text;
+    
+    // filter out user ids from text
+    const filteredText = event.text.replace(/<@([A-Z])\w+>/g, "")
+    
+    console.log(filteredText);
   } catch (error) {
     console.error(error);
   }
