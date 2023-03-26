113323494e692d356c5aff946f22936b6a6f511d
Glitch (bodzii-slack-app) <none>
2023-03-26 13:45:28 +0000

    🍄🐁 Checkpoint ./services/gpt.js:44950819/2027 ./listeners/events/app_mention.js:44950819/5194

--- listeners/events/app_mention.js
+++ listeners/events/app_mention.js
@@ -1,28 +1,35 @@
 const { getUsers } = require("../../database/db.js");
+const ChatGPT = require("../../services/gpt.js");
 
-
-const appMention = async ({ event, client, body }) => {
-
+const appMention = async ({ event, client, body, say }) => {
   try {
-    
-    if(!event || !event.text) {
-      console.error("event text not provided or empty")
+    if (!event || !event.text) {
+      console.error("event text not provided or empty");
       return;
     }
     // get text
     const text = event.text;
-    
+
     // filter out user ids from text
-    const filteredText = event.text.replace(/<@([A-Z])\w+>/g, "")
+    const filteredText = event.text.replace(/<@([A-Z])\w+>/g, "");
+
+    const chatGPT = new ChatGPT(process.env.CHATGPT_API_KEY);
+    const messages = [
+      {
+        role: "system",
+        content:
+          "You are a friendly chat bot designed to answer specific questions in any category and summarize threads.",
+      },
+      { role: "user", content: text },
+    ];
+    const resData = chatGPT.sendCompletion(messages);
+    const resMessage = resData[0].message.content;
     
-    
-    
+    await say("")
     
   } catch (error) {
     console.error(error);
   }
 };
 
-
-
-module.exports = {appMention};
+module.exports = { appMention };
--- services/gpt.js
+++ services/gpt.js
@@ -5,23 +5,22 @@ class ChatGPT {
     this.apiKey = apiKey;
   }
 
-  async sendCompletion(prompt) {
-    await axios.post(
-      process.env.CHATGPT_COMPLETIONS_URL,
+  async sendCompletion(messages) {
+    const res = await axios.post(
+      process.env.CHATGPT_CHAT_URL,
       {
         model: "gpt-3.5-turbo",
-        prompt: prompt,
-        max_tokens: 7,
-        temperature: 0,
+        messages: messages,
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.CHATGPT_API_KEY}`
-        }
+          Authorization: `Bearer ${this.apiKey}`,
+        },
       }
     );
+
+    return res.data;
   }
 }
 
-
-module.exports = ChatGPT
+module.exports = ChatGPT;
