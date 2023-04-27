d6d131283a7b944b52fb316d5e25c9a8f80b378b
Glitch (bodzii-slack-app) <none>
2023-04-27 12:47:12 +0000

    📚💾 Checkpoint ./listeners/events/index.js:44950819/760 ./listeners/events/app_home_opened.js:44950819/8292

---
+++ listeners/events/app_home_opened.js
@@ -0,0 +1,38 @@
+const { getUsers } = require("../../database/db.js");
+const ChatGPT = require("../../services/gpt.js");
+
+const appHomeOpened = async ({ event, client, body, say, logger }) => {
+  try {
+    // Call views.publish with the built-in client
+    const result = await client.views.publish({
+      // Use the user ID associated with the event
+      user_id: event.user,
+      view: {
+        // Home tabs must be enabled in your app configuration page under "App Home"
+        type: "home",
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Hey, <@" + event.user + ">*",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.",
+            },
+          },
+        ],
+      },
+    });
+
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+module.exports = { appHomeOpened };
--- listeners/events/index.js
+++ listeners/events/index.js
@@ -1,5 +1,7 @@
 const { appMention } = require("./app_mention.js");
+const { appHomeOpened } = require("./app_home_opened.js");
 
 module.exports.register = (app) => {
   app.event("app_mention", appMention);
+  app.event("app_home_opened", appHomeOpened);
 };
