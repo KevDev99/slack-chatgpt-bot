ca853d32c868797a5ffddb7b390fbaf2725ff82d
Glitch (bodzii-slack-app) <none>
2023-04-28 18:31:47 +0000

    💠👡 Checkpoint ./listeners/actions/send_bot_message.js:44950819/1869

--- listeners/actions/send_bot_message.js
+++ listeners/actions/send_bot_message.js
@@ -7,16 +7,16 @@ const sendBotMessage = async ({ body, ack, client, event, state }) => {
       await ack();
       return;
     }
-    
-    console.log(body);
-    
+
+    await ack();
+
     // extract value
     const messageInputValue =
       body.view.state.values["message_block_input"]["send_bot_message"].value;
     const botId = "U04V9UQ9RK7"; //body.view["bot_id"];
 
     // send message with mentioning the bot
-    client.chat.postMessage({
+    const response = await client.chat.postMessage({
       channel: body.user.id,
       as_user: true,
       blocks: [
@@ -27,7 +27,7 @@ const sendBotMessage = async ({ body, ack, client, event, state }) => {
       ],
     });
 
-    await ack();
+    console.log(response);
   } catch (err) {
     console.error(err);
   }
